c653fabf02438b549c38d7bcd6ed667097c11f2a
Sangay Thinley <thinley.sangay@selise.ch>
2023-02-14 17:17:57 +0600

    remove unuse import

--- src/components/main-navigation/main-navigation.js
+++ src/components/main-navigation/main-navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from "./main-navigation.module.css";
 
 function MainNavigation() {
