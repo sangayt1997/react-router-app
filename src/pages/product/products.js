2a424a06f2a832881038df254ff9fd90ddf14a04
Sangay Thinley <thinley.sangay@selise.ch>
2023-02-14 18:11:29 +0600

    adding link for dynamic routes

--- src/pages/product/products.js
+++ src/pages/product/products.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Products() {
+    const PRODUCTS = [
+        { id: 'p1', title: 'Product 1' },
+        { id: 'p2', title: 'Product 2' },
+        { id: 'p3', title: 'Product 3' },
+    ]
     return (
         <>
             <h1>Product page</h1>
             <ul>
-                <li>Product 1</li>
-                <li>Product 2</li>
-                <li>Product 3</li>
+                {PRODUCTS.map((items) => (
+                    <li key={items.id}>
+                        <Link to={`/products/${items.id}`}>
+                            {items.title}
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </>
     );
