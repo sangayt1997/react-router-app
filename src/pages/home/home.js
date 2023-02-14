0222b3c7620ed2105e621bc79e8bd3a1826bc6e4
Sangay Thinley <thinley.sangay@selise.ch>
2023-02-14 17:46:38 +0600

    dynamic navigation in routes

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
+    const navigate = useNavigate();
+    const navigateToProduct = () => {
+        navigate('/products')
+    }
     return (
         <>
             <h1>Home page</h1>
             <p>
                 Go to <Link to="/products">the list of products</Link>
             </p>
+            <button onClick={navigateToProduct}>Navigate to Products</button>
         </>
     );
 }
