9eaafa4a3ab8da54011a02639060cbdc8b97a12e
Sangay Thinley <thinley.sangay@selise.ch>
2023-02-14 15:19:51 +0600

    implement simple router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/home";
+import Products from "./pages/product/products";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: '/products',
+        element: <Products />
+    },
+]);
+
 function App() {
-  return <div></div>;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
